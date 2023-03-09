131f2af4f9732457deb1862aa484246dc54c58ad
virido <yannick.luedemann@gmx.de>
2023-03-09 14:58:34 +0100

    ExportMap.java Dummy

---
+++ src/kom/feuerwehr/task/ExportMap.java
@@ -0,0 +1,13 @@
+package kom.feuerwehr.task;
+
+import java.io.IOException;
+
+public class ExportMap {
+	
+	 public void export( ) throws IOException {
+		 
+		 System.out.println("doing export");
+		 
+	 }
+
+}
